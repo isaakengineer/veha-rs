f1b3bf75779f85a95b5a150b8685751bdfdd5027
Isaak Engineer <i-engineer@tutamail.com>
2025-04-05 23:22:36 +0200

    moving towards the original design

--- src/motor/sqlite.rs
+++ src/motor/sqlite.rs
@@ -2,9 +2,13 @@ use log::{debug, error, info, warn};
 use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
 use quick_xml::reader::Reader;
 use quick_xml::writer::Writer;
+use rusqlite::types::ValueRef;
 use rusqlite::{Connection, Result, Row};
+use serde::Serialize;
 use serde_json::{Map, Value};
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
 
 use crate::utils;
 
@@ -21,35 +25,56 @@ fn get_table_schema(conn: &Connection, table_name: &str) -> Result<Vec<(String, 
     Ok(schema)
 }
 
-fn query_to_json(
+fn row_to_json(columns: Vec<String>, row: &Row) -> Result<Map<String, Value>> {
+    let mut map = Map::new();
+    for (i, column) in columns.iter().enumerate() {
+        let value: Value = match row.get_ref(i)? {
+            rusqlite::types::ValueRef::Null => Value::Null,
+            rusqlite::types::ValueRef::Integer(v) => Value::Number(v.into()),
+            rusqlite::types::ValueRef::Real(v) => {
+                serde_json::Number::from_f64(v).map_or(Value::Null, Value::Number)
+            }
+            rusqlite::types::ValueRef::Text(v) => {
+                Value::String(String::from_utf8_lossy(v).to_string())
+            }
+            rusqlite::types::ValueRef::Blob(v) => {
+                Value::Array(v.iter().map(|&b| Value::Number(b.into())).collect())
+            }
+        };
+        map.insert(column.clone(), value);
+    }
+    Ok(map)
+}
+
+fn row_to_hashmap(columns: Vec<String>, row: &Row) -> rusqlite::Result<HashMap<String, String>> {
+    let mut map = HashMap::new();
+    for (i, column) in columns.iter().enumerate() {
+        let value: String = match row.get_ref(i)? {
+            ValueRef::Null => "null".to_string(),
+            ValueRef::Integer(v) => v.to_string(),
+            ValueRef::Real(v) => v.to_string(),
+            ValueRef::Text(v) => String::from_utf8_lossy(v).to_string(),
+            ValueRef::Blob(v) => v
+                .iter()
+                .map(|&b| format!("{:02x}", b))
+                .collect::<Vec<String>>()
+                .join(""),
+        };
+        map.insert(column.to_string(), value);
+    }
+    Ok(map)
+}
+
+fn query_to_map(
     conn: &Connection,
     query: String,
     table_name: &str,
-) -> Result<Vec<Map<String, Value>>> {
+) -> Result<Vec<HashMap<String, String>>> {
     let schema = get_table_schema(conn, table_name)?;
     let columns: Vec<String> = schema.iter().map(|(name, _)| name.clone()).collect();
 
     let mut stmt = conn.prepare(&query)?;
-    let rows = stmt.query_map([], |row| {
-        let mut map = Map::new();
-        for (i, column) in columns.iter().enumerate() {
-            let value: Value = match row.get_ref(i)? {
-                rusqlite::types::ValueRef::Null => Value::Null,
-                rusqlite::types::ValueRef::Integer(v) => Value::Number(v.into()),
-                rusqlite::types::ValueRef::Real(v) => {
-                    serde_json::Number::from_f64(v).map_or(Value::Null, Value::Number)
-                }
-                rusqlite::types::ValueRef::Text(v) => {
-                    Value::String(String::from_utf8_lossy(v).to_string())
-                }
-                rusqlite::types::ValueRef::Blob(v) => {
-                    Value::Array(v.iter().map(|&b| Value::Number(b.into())).collect())
-                }
-            };
-            map.insert(column.clone(), value);
-        }
-        Ok(map)
-    })?;
+    let rows = stmt.query_map([], |row| row_to_hashmap(columns.clone(), row))?;
 
     let mut result = Vec::new();
     for row in rows {
@@ -58,15 +83,25 @@ fn query_to_json(
     Ok(result)
 }
 
+fn write_hashmap_to_file(
+    map: &Vec<HashMap<String, String>>,
+    file_path: &str,
+) -> std::io::Result<()> {
+    let json = serde_json::to_string_pretty(map).expect("Failed to serialize HashMap");
+    let mut file = File::create(file_path)?;
+    file.write_all(json.as_bytes())?;
+    Ok(())
+}
+
 fn process(
     sqlite_src: String,
     query: String,
     table_name: String,
-) -> Result<String, Box<dyn std::error::Error>> {
+) -> Result<Vec<HashMap<String, String>>, Box<dyn std::error::Error>> {
     let conn = Connection::open(sqlite_src)?;
-    let json_data = query_to_json(&conn, query, table_name.as_str())?;
-    let json_str = serde_json::to_string_pretty(&json_data)?;
-    Ok(json_str)
+    let map_data = query_to_map(&conn, query, table_name.as_str())?;
+    // let json_str = serde_json::to_string_pretty(&json_data)?;
+    Ok(map_data)
 }
 
 pub fn lesen(
@@ -81,6 +116,7 @@ pub fn lesen(
 
     let mut fahne_sqlanfang = false;
     let mut fahne_sqlende = false;
+    let mut fahne_backend = false;
 
     let mut sqlite_content = String::new();
     let mut sqlite_src = String::new();
@@ -90,10 +126,13 @@ pub fn lesen(
         match xmlleser.read_event() {
             Ok(Event::Start(e)) if e.local_name().as_ref() == b"sqlite" => {
                 fahne_sqlanfang = true;
-                if let Some(src) = utils::attributenwert_lesen(e.clone(), "src") {
-                    sqlite_src = src;
-                    if let Some(table) = utils::attributenwert_lesen(e.clone(), "table") {
-                        sqlite_table = table;
+                if utils::attribut_vorhanden(e.clone(), "backend") {
+                    fahne_backend = true;
+                    if let Some(src) = utils::attributenwert_lesen(e.clone(), "src") {
+                        sqlite_src = src;
+                        if let Some(table) = utils::attributenwert_lesen(e.clone(), "table") {
+                            sqlite_table = table;
+                        }
                     }
                 }
             }
@@ -112,10 +151,10 @@ pub fn lesen(
         }
     }
 
-    if fahne_sqlanfang && fahne_sqlende {
+    if fahne_backend && fahne_sqlanfang && fahne_sqlende {
         info!("Content of <sqlite> tag: {}", sqlite_content);
-        let json_str = process(sqlite_src, sqlite_content, sqlite_table).unwrap();
-        std::fs::write("output.json", json_str)?; // TODO: move it
+        let map = process(sqlite_src, sqlite_content, sqlite_table).unwrap();
+        write_hashmap_to_file(&map, "./output.json"); // TODO: move it
     } else {
         warn!("No <sqlite> tag found or it is not properly closed.");
     }
